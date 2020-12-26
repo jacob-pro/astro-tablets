b37ba7594c5204776096993e968438eb0402183c
Jacob Halsey <jacob@jhalsey.com>
2020-12-26 11:34:09 +0000

    month b

--- query/bm32312.sql
+++ query/bm32312.sql
@@ -34,14 +34,14 @@ SELECT *,
        (CASE WHEN SaturnLastAppearance14 IS NOT NULL THEN 1 ELSE 0 END) +
        (CASE WHEN SaturnInPisces14 IS NOT NULL THEN 1 ELSE 0 END) +
        (CASE WHEN MarsStationary17 IS NOT NULL THEN 1 ELSE 0 END) +
-       (CASE WHEN MarsScorpionHead17 IS NOT NULL THEN 1 ELSE 0 END) as SCORE
+       (CASE WHEN MarsScorpionHead17 IS NOT NULL THEN 1 ELSE 0 END) as MONTH_A_SCORE
 FROM (SELECT sunset, year,
     (SELECT time FROM events WHERE body="Mercury" AND event="MorningLast"
         AND time >= (sunset + 13 -  5) AND time <= (sunrise + 13 + 5) LIMIT 1) as MercuryMorningLast14,
     (SELECT MIN(angle) FROM separations WHERE from_body="Mercury" AND to_body="58 Piscium"
         AND angle < 50 AND time >= (sunset + 13) AND time <= (sunrise + 13) LIMIT 1) as MercuryInPisces14,
     (SELECT time FROM events WHERE body="Saturn" AND event="LastAppearance"
-        AND time >= (sunset + 13 - 5) AND time <= (sunrise + 13 + 5) LIMIT 1) as SaturnLastAppearance14,
+        AND time >= (sunset + 13 - 10) AND time <= (sunrise + 13 + 10) LIMIT 1) as SaturnLastAppearance14,
     (SELECT MIN(angle) FROM separations WHERE from_body="Saturn" AND to_body="58 Piscium"
         AND angle < 50 AND time >= (sunset + 13) AND time <= (sunrise + 13) LIMIT 1) as SaturnInPisces14,
     (SELECT time FROM events WHERE body="Mars" AND event="Stationary"
@@ -49,5 +49,24 @@ FROM (SELECT sunset, year,
     (SELECT MIN(angle) FROM separations WHERE from_body="Mars" AND to_body="Antares"
         AND angle < 10 AND time >= (sunset + 16) AND time <= (sunrise + 16) LIMIT 1) as MarsScorpionHead17
     FROM potential_months)
-ORDER BY SCORE DESC;
+ORDER BY MONTH_A_SCORE DESC;
 
+SELECT *,
+       (CASE WHEN MorningFirst5 IS NOT NULL THEN 1 ELSE 0 END) +
+       (CASE WHEN MercuryInPisces5 IS NOT NULL THEN 1 ELSE 0 END) +
+       (CASE WHEN VenusBehindMars19 IS NOT NULL THEN 1 ELSE 0 END) +
+       (CASE WHEN MarsInAries19 IS NOT NULL THEN 1 ELSE 0 END) +
+       (CASE WHEN MarsInAries20 IS NOT NULL THEN 1 ELSE 0 END) as MONTH_B_SCORE
+FROM (SELECT sunset, year,
+    (SELECT time FROM events WHERE body="Mercury" AND event="MorningFirst"
+        AND time >= (sunset + 4 -  5) AND time <= (sunrise + 4 + 5) LIMIT 1) as MorningFirst5,
+    (SELECT MIN(angle) FROM separations WHERE from_body="Mercury" AND to_body="58 Piscium"
+        AND angle < 50 AND time >= (sunset + 4) AND time <= (sunrise + 4) LIMIT 1) as MercuryInPisces5,
+    (SELECT MIN(angle) FROM separations WHERE from_body="Venus" AND to_body="Mars" AND position="behind"
+        AND angle < 5 AND time >= (sunset + 18) AND time <= (sunrise + 19) LIMIT 1) as VenusBehindMars19,
+    (SELECT MIN(angle) FROM separations WHERE from_body="Mars" AND to_body="Nu Arietis"
+        AND angle < 30 AND time >= (sunset + 18) AND time <= (sunrise + 18) LIMIT 1) as MarsInAries19,
+    (SELECT MIN(angle) FROM separations WHERE from_body="Mars" AND to_body="Nu Arietis"
+        AND angle < 20 AND time >= (sunset + 19) AND time <= (sunrise + 19) LIMIT 1) as MarsInAries20
+    FROM potential_months)
+ORDER BY MONTH_B_SCORE DESC;
