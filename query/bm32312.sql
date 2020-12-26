85653f3544136522c83a9f6adbe943c670177052
Jacob Halsey <jacob@jhalsey.com>
2020-12-26 10:16:50 +0000

    work on query

--- query/bm32312.sql
+++ query/bm32312.sql
@@ -1,11 +1,31 @@
-/*
-* Generate "numbers" / "tally" table of computed years
-* (`days` table will always have more rows than the total years)
-*/
-DROP TABLE IF EXISTS years;
-CREATE TEMPORARY TABLE years (
+/* Table of possible year start days */
+DROP TABLE IF EXISTS potential_years;
+CREATE TEMPORARY TABLE potential_years (
+    nisan_1 FLOAT,
     year INT
 );
-INSERT INTO years
-SELECT ROW_NUMBER() OVER() + (SELECT start_year FROM db_info LIMIT 1) - 1 AS `year`
-FROM days LIMIT 1 + (SELECT end_year FROM db_info LIMIT 1) - (SELECT start_year FROM db_info LIMIT 1);
+INSERT INTO potential_years
+SELECT days.sunset as nisan_1, days.year as year
+FROM events equinox
+INNER JOIN
+    days ON days.sunset >= (equinox.time - 31) and days.sunset <= (equinox.time + 31) and days.first_visibility==1
+WHERE equinox.event="VernalEquinox" and equinox.body="Sun"
+AND days.year <= (SELECT end_year FROM db_info LIMIT 1);
+
+/* Table of possible months in each possible year */
+DROP TABLE IF EXISTS potential_months;
+CREATE TEMPORARY TABLE potential_months (
+    nisan_1 FLOAT,
+    year INT,
+    sunset FLOAT
+);
+INSERT INTO potential_months
+SELECT nisan_1, potential_years.year, days.sunset
+FROM potential_years
+INNER JOIN days ON days.first_visibility==1
+WHERE days.sunset >= nisan_1
+AND days.sunset <= (SELECT MAX(sunset)
+FROM (SELECT sunset FROM days WHERE days.first_visibility==1 AND sunset >= nisan_1 ORDER BY sunset LIMIT 12));
+
+SELECT * FROM potential_months
+
