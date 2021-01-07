db4ba72147a6dc65cc6f7e109d831ce9c8c34a9a
Jacob Halsey <jacob@jhalsey.com>
2021-01-07 23:06:18 +0000

    fix deploy

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,9 @@ REGISTRY=registry.jhalsey.com
 TAG=$REGISTRY/astro-tablets:latest
 
 GIT_HASH=$(git rev-parse --short HEAD)
+set +e
 git diff --exit-code --quiet
+set -e
 GIT_MODIFIED=$?
 
 $DOCKER build -t $TAG --build-arg GIT_HASH="${GIT_HASH}" --build-arg GIT_MODIFIED="${GIT_MODIFIED}" .
