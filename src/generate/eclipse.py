f0a20fd5e94ae69ea3386f60639f4ed7b1b0b219
Jacob Halsey <jacob@jhalsey.com>
2021-05-25 00:52:07 +0100

    eclipse length

--- src/generate/eclipse.py
+++ src/generate/eclipse.py
@@ -1,14 +1,73 @@
+from dataclasses import dataclass
+from typing import *
+
 from skyfield import eclipselib
+from skyfield.searchlib import find_discrete
 from skyfield.timelib import Time
 
 from data import AstroData
-from util import TimeValue
+
+## https://github.com/skyfielders/python-skyfield/issues/545
 
 
-def lunar_eclipses_in_range(data: AstroData, start: Time, end: Time):
-    t, y, details = eclipselib.lunar_eclipses(start, end, data.ephemeris)
-    for ti, yi in zip(t, y):
-        print(TimeValue(ti.tt).string(data.timescale),
-              'y={}'.format(yi),
-              eclipselib.LUNAR_ECLIPSES[yi])
+@dataclass
+class SkyfieldEclipse:
+    time: Time
+    type: str
+    closest_approach_radians: float
+    moon_radius_radians: float
+    penumbra_radius_radians: float
+    umbra_radius_radians: float
+
+    def upgrade(self, data: AstroData):
+        sdict = dict(((s.center, s.target), s.spk_segment) for s in data.ephemeris.segments)
+        sun = sdict[0, 10]
+        earth_barycenter = sdict[0, 3]
+        earth = sdict[3, 399]
+        moon = sdict[3, 301]
+
+        def f(t):
+            from skyfield.functions import angle_between
+
+            jd, fr = t.whole, t.tdb_fraction
+            b = earth_barycenter.compute(jd, fr)
+            e = earth.compute(jd, fr)
+            m = moon.compute(jd, fr)
+            s = sun.compute(jd, fr)
+
+            earth_to_sun = s - b - e
+            moon_to_earth = e - m
+
+            angle = angle_between(earth_to_sun, moon_to_earth)
+            return angle < self.penumbra_radius_radians + self.moon_radius_radians
+
+        f.step_days = 0.01
+        times, values = find_discrete(data.timescale.tt_jd(self.time.tt - 1), data.timescale.tt_jd(self.time.tt + 1), f)
+        assert len(times) == 2
+        assert (values == [1, 0]).all()
+        assert times[0].tt < self.time.tt
+        assert times[1].tt > self.time.tt
+
+        return Eclipse(start_time=times[0], end_time=times[1], base=self)
+
+
+@dataclass
+class Eclipse:
+    start_time: Time
+    end_time: Time
+    base: SkyfieldEclipse
+
+
+def lunar_eclipses_in_range(data: AstroData, start: Time, end: Time) -> List[Eclipse]:
+    times, types, details = eclipselib.lunar_eclipses(start, end, data.ephemeris)
+    bases = []  # type: List[SkyfieldEclipse]
+    for idx, _ in enumerate(times):
+        bases.append(SkyfieldEclipse(time=times[idx],
+                                     type=eclipselib.LUNAR_ECLIPSES[types[idx]],
+                                     closest_approach_radians=details['closest_approach_radians'][idx],
+                                     moon_radius_radians=details['moon_radius_radians'][idx],
+                                     penumbra_radius_radians=details['penumbra_radius_radians'][idx],
+                                     umbra_radius_radians=details['umbra_radius_radians'][idx]))
+    return list(map(lambda x: x.upgrade(data), bases))
+
 
--- src/test/test_eclipse.py
+++ src/test/test_eclipse.py
@@ -1,6 +1,7 @@
 from unittest import TestCase
 
 from generate.eclipse import *
+from util import TimeValue, diff_mins
 
 
 class EclipseTest(TestCase):
@@ -11,6 +12,12 @@ class EclipseTest(TestCase):
 
     def test_lunar_eclipse(self):
         t0 = self.data.timescale.ut1(-631, 5, 1)
-        t1 = self.data.timescale.ut1(-631, 6, 1)
-        lunar_eclipses_in_range(self.data, t0, t1)
+        t1 = self.data.timescale.ut1(-600, 6, 1)
+        eclipses = lunar_eclipses_in_range(self.data, t0, t1)
+
+        for e in eclipses:
+            print("start", TimeValue(e.start_time.tt).string(self.data.timescale), "end",
+                  TimeValue(e.end_time.tt).string(self.data.timescale), "max",
+                  TimeValue(e.base.time.tt).string(self.data.timescale), "length",
+                  diff_mins(e.start_time, e.end_time))
 
